e35a35aa57cef9d21ab5844df7cf289a542a907c
ChefOmarDee <okhan@vazkor.com>
2023-09-22 10:05:00 -0400

    added react multipage via react router

---
+++ src/Comps/Home2.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Home2 = () => {
+	return (
+		<React.Fragment>
+			<div>Aarons ugly ass</div>
+			<h1>UF Grad</h1>
+			<div className="card">number of L's Roger has is 8</div>
+			<a href="/Home"></a>
+		</React.Fragment>
+	);
+};
+export default Home2;
